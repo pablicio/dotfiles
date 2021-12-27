e6e5334218cf97f086ee1c1e53705c2189c52c2c
unknown <pabliciotjg@gmail.com>
2021-12-27 10:15:38 -0300

    adicionando arquivos dotfiles

---
+++ README.md
@@ -0,0 +1,50 @@
+My Dotfiles
+====================
+
+## Source of inspiration
+
+My dotfiles are based on following amazing dotfiles:
+
+[Skwp Dotfiles](http://github.com/skwp/dotfiles)
+
+[ThoughtBot Dotfiles](http://github.com/thoughtbot/dotfiles)
+
+[CampusCode Dotfiles](https://github.com/campuscode/cc_dotfiles)
+
+## Requirements
+
+* Zsh
+* [Ag](https://github.com/ggreer/the_silver_searcher)
+* [ITerm 2 (Mac)](https://www.iterm2.com/index.html)
+* MVim (Mac) or GVim (Linux)
+* [Solarized](http://ethanschoonover.com/solarized)
+* Tmux
+
+## Install
+
+Run follow command:
+
+```
+sh -c "`curl -fSs https://raw.githubusercontent.com/marciocavarzeri/dotfiles/master/install.sh`"
+```
+
+Type your password to change your default shell to `zsh`
+
+## Docs
+
+[Vim Key Mapping](Vim.md)
+
+[Tmux Key Mapping](Tmux.md)
+
+#### It's easy to make your customizations
+
+Place your customizations in the following files:
+
+* .aliases.local
+* .secrets
+* .zshrc.local
+* .vimrc.local
+* .zshenv.local
+* .plugin.vim.local
+* .tmux.conf.local
+* .gitconfig.local
---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ ! -d "$HOME/.dotfiles" ]
+then
+  echo "Installing Dotfiles"
+  echo "We'll install:"
+  echo "  - tmux"
+  echo "  - silver searcher"
+  echo "  - zsh"
+  echo "  - vim (vim-gnome | macvim)"
+  echo "  - google-chrome (mac)"
+  echo "  - iterm2 (mac)"
+  echo "  - atom (mac)"
+
+  case "$(uname -s)" in
+    Linux)
+      sudo apt-get install -y git curl gnupg build-essential
+      gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+      curl -sSL https://get.rvm.io | bash -s stable --ruby
+      sudo usermod -a -G rvm `whoami`
+      sudo apt-get install ruby
+      ;;
+    Darwin )
+      gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+      curl -sSL https://get.rvm.io | bash -s stable --ruby
+      sudo usermod -a -G rvm `whoami`
+      ;;
+    CYGWIN* | MSYS*)
+      echo 'You are using a Windows machine which is not recommended to use with our' \
+           ' dotfiles.'
+      echo 'You can clone our repository and install it manually.'
+      return
+      ;;
+    *)
+      echo 'Operational system not recognized, aborting installation'
+      return
+      ;;
+  esac
+  git clone --depth=10 https://github.com/marciocavarzeri/dotfiles.git "$HOME/.dotfiles"
+  cd "$HOME/.dotfiles"
+  rake install
+else
+  echo "You already have Dotfiles installed."
+fi
